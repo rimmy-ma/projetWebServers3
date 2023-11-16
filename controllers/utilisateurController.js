b660fe43077435f4b4a645f8bc141b8a7c50fa38
andywhodat <2695845@collegelacite.ca>
2023-11-15 22:12:20 -0500

    ...hashing mot de passe

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -1,32 +1,42 @@
-// controllers/utilisateurController.js
-const db = require('../models'); // The path may vary depending on your structure.
+const db = require('../models');
 const Utilisateur = db.utilisateurs;
+const bcrypt = require('bcrypt');
 
 // Ajouter un nouvel utilisateur
 exports.create = (req, res) => {
-  // Validate request...
-  
-  // Create a User
-  const utilisateur = {
-    nom: req.body.nom,
-    prenom: req.body.prenom,
-    email: req.body.email,
-    motDePasse: req.body.motDePasse, // Think about hashing the password!
-  };
+  // Valider la requête...
 
-  // Save User in the database
-  Utilisateur.create(utilisateur)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
+  // Hacher le mot de passe avant de le sauvegarder
+  bcrypt.hash(req.body.motDePasse, 10, (err, hash) => {
+    if (err) {
       res.status(500).send({
-        message:
-          err.message || "Une erreur s'est produite lors de la création de l'utilisateur."
+        message: "Erreur lors du hachage du mot de passe."
       });
-    });
+      return;
+    }
+
+    // Créer un utilisateur avec le mot de passe haché
+    const utilisateur = {
+      nom: req.body.nom,
+      prenom: req.body.prenom,
+      email: req.body.email,
+      motDePasse: hash, // Stocker le mot de passe haché
+    };
+
+    // Sauvegarder l'utilisateur dans la base de données
+    Utilisateur.create(utilisateur)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Une erreur s'est produite lors de la création de l'utilisateur."
+        });
+      });
+  });
 };
 
-// Rest of CRUD operations...
+// Reste des opérations CRUD...
 
 module.exports = exports;
